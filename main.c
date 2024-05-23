c58d2a9c14e127715a54e33a7d3745e60a062245
jukoo <jUmarB@protonmail.com>
2024-05-23 14:41:46 +0000

    information gathering  about active device interface

--- main.c
+++ main.c
@@ -14,20 +14,20 @@
 int main (int ac , char **av ) 
 {
 
-  if (ac == 1)   
-    errx(~0 , "<ipv4 address>"); 
 
   char errbuf[PCAP_ERRBUF_SIZE] ={0} ; 
   pcap_if_t  * netdevs ;
   bpf_u_int32 netip , maskip ; 
   pcap_t  *handler ;
-
-
+ 
+  /* 
+  if (ac == 1)   
+    errx(~0 , "<ipv4 address>"); 
   char *ip = av[ac-1] ; 
   
   if (net_is_valid_ipv4_addr(av[ac-1]) != 0 )   
     errx(~0 , "Not a valid ip address") ; 
-
+  */ 
  
   int status =   pcap_findalldevs(&netdevs ,errbuf) ;
   
@@ -41,23 +41,28 @@ int main (int ac , char **av )
   if (!idevices)  
     errx(-1, "No Connected adaptater found") ; 
 
-   list_inetdevs(idevices) ; 
-  /* 
-
-  if(PCAP_ERROR == pcap_lookupnet(device , &netip , &maskip , errbuf)) {
-    free(device) ; 
-    pcap_freealldevs(netdevs) ;
-    errx(PCAP_ERROR , "pcap_lookupnet: %s" , errbuf) ; 
-  }
-
-   * */
-   struct __raw_iproto  rip ; 
-   (void *)net_translate(&rip , 2,  netip , maskip) ; 
-
- 
-  printf("ipv4 : %s\n",  rip.ipv4) ; 
-  printf("net mask : %s\n" , rip.subnet_mask) ; 
   
+  list_inetdevs(idevices) ; 
+  
+  char *idevname = net_get_device_name(idevices); 
+
+  idev_info_t  * idevinfo =  (void *) 0 ;  
+ 
+  
+  idevinfo = net_get_idev_info(idevname);  
+                                        
+  //show_idevinfo(idevinfo) ;  
+  
+  if (idevinfo) { 
+    printf("--> ip  %s \n", idevinfo->ipv4netnum) ; 
+    printf("--> subnet  %s \n", idevinfo->subnet_mask) ; 
+    printf("--> class type %s\n" , idevinfo->class_type) ; 
+  } 
+
+
+   return 0 ; 
+
+   //! ADD filter  here 
   //handler = net_stream_on(null ,) ; // if null start on first devices  
   handler =  pcap_open_live(idevices  , BUFSIZ,  0 ,10, errbuf)  ; 
   if (!handler){
--- net.c
+++ net.c
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>  
 #include <netinet/ip.h>
+#include <netinet/in.h> 
 #include <netinet/ip_icmp.h> 
 #include <pcap/pcap.h> 
 #include <string.h> 
@@ -25,21 +26,14 @@ bint net_is_valid_ipv4_addr(char * restrict ipv4_addr_string)
 struct __active_inet_devices * 
 net_found_active_interface(pcap_if_t  * raw_net_interface , struct __active_inet_devices * inetdevs)   
 {
-  
+  int interface_status_check = 0 ; 
+
   while (raw_net_interface  != nullable){
     
-    int  interface_status_check =  raw_net_interface->flags  & PCAP_IF_CONNECTION_STATUS ; 
+    interface_status_check =  raw_net_interface->flags  & PCAP_IF_CONNECTION_STATUS ;
     
-    if (interface_status_check ==  PCAP_IF_CONNECTION_STATUS_CONNECTED) { 
-      char *connected_inetdev = raw_net_interface->name ;
-      bpf_u_int32 netp  , maskp ; 
-      //! check  if the device has ipv4 address   
-      int status =  pcap_lookupnet (connected_inetdev  ,  &netp , &maskp ,  error_buff) ;
-      
-      if  (PCAP_ERROR ==  status) {
-        raw_net_interface = raw_net_interface->next ; 
-        continue ; 
-      }
+    if (interface_status_check ==  PCAP_IF_CONNECTION_STATUS_CONNECTED) {  
+
 
       struct __active_inet_devices * new_active_inetdev =  (struct __active_inet_devices *) \
                                malloc(sizeof(*new_active_inetdev)); 
@@ -49,10 +43,11 @@ net_found_active_interface(pcap_if_t  * raw_net_interface , struct __active_inet
         return nullable ; 
       }
 
-      new_active_inetdev->idev = strdup(connected_inetdev) ;  
+      new_active_inetdev->idev = strdup(raw_net_interface->name) ;   
       
       inetdevs = append_inetdev(inetdevs ,   new_active_inetdev) ; 
-    } 
+
+    } //!  endif (interface_status_check == PCAP_IF_CONNECTION_STATUS_CONNECTED)  
     
     raw_net_interface = raw_net_interface->next ;  
   }
@@ -62,37 +57,46 @@ net_found_active_interface(pcap_if_t  * raw_net_interface , struct __active_inet
 
 }
 
-
-struct __raw_iproto *  net_translate( struct __raw_iproto  * rip  ,int index ,  ... )
-{ 
-
-  struct in_addr *address =  (struct in_addr *) malloc(sizeof(struct in_addr) * index) ; 
-  if (!address)  
-    return nullable ; 
-
-  bpf_u_int32 ip ; 
-
-  va_list ap ; 
-  va_start(ap , index) ; 
-
-  int i = 0 ; 
-  while (  i <  index ) { 
-    ip= va_arg(ap ,  bpf_u_int32) ;
-    address[i].s_addr = ip  ; 
-    if (i == 0){
-      memcpy(rip->ipv4 , inet_ntoa(address[i]) ,NET_IPV4_LENGTH) ; 
-    } 
-    if (i == 1) {
-      memcpy(rip->subnet_mask  , inet_ntoa(address[i]) , NET_IPV4_LENGTH) ; 
-    } 
-    explicit_bzero((char*) address ,  sizeof(struct in_addr ) * index) ; 
-    i++ ; 
-  }
+struct __idev_info_t  * 
+net_get_idev_info(const char  * restrict   idevname) 
+{
+   idev_info_t *  idev_info = (idev_info_t *) malloc(sizeof(*idev_info)) ; 
+   if (!idev_info) 
+     return nullable ; 
+  
+   bpf_u_int32  netip, subnetip ; 
+  
    
-  va_end(ap) ; 
-  free(address) ; 
-  return rip ; 
+   int status = pcap_lookupnet(idevname , &netip , &subnetip ,  error_buff) ; 
+   if (PCAP_ERROR == status){ 
+     free(idev_info) ; 
+     fprintf(stderr , "%s\n" , error_buff) ; 
+     return nullable; 
+   }
+
+   struct  in_addr  address[2] ; 
+ 
+   printf("subnet mask   %i  ::   %x\n " , subnetip ,subnetip) ;
+
+   int subnetclass =    SUBN_MASK & subnetip  ; 
+
+   address[0].s_addr = netip  ; 
+   address[1].s_addr = subnetip ;  
+   
+   
+  
+   memcpy(idev_info->ipv4netnum , inet_ntoa( *(address+0) ) , NET_IPV4_LENGTH) ; 
+   memcpy(idev_info->subnet_mask , inet_ntoa( *(address+1) ) , NET_IPV4_LENGTH) ; 
+   idev_info->class_type  =  _GSTRCLS(subnetclass) ; 
+
+
+   if (!idev_info->ipv4netnum || !idev_info->subnet_mask) 
+     fprintf(stderr , "Not  able to fecth  all information about this device %s\n", idevname) ; 
+
+  
+   return  idev_info ; 
 }
+
 
 
 
@@ -100,11 +104,13 @@ void net_handler(u_char * device , const struct pcap_pkthdr * pkhdr ,  const u_c
 {
  
   printf("size of raw packet bytes %i\n" , strlen(raw_packet_bytes)) ; 
- 
+  
+  //!Extract  Ethernet Trame from  raw_packet_bytes 
   struct ether_header  *ethn  = (struct ether_header *) raw_packet_bytes ;
   
   uint16_t  eth_type =  ntohs(ethn->ether_type) ;
   if (eth_type == ETHERTYPE_IP)  {
+     //! Extract  IP Datagrame 
      struct iphdr * ip = (struct iphdr *) (raw_packet_bytes   + sizeof(struct ether_header)) ; 
      char s[10] ={0} ; 
      sprintf(s ,"::%i\n",   ip->protocol) ;
@@ -128,13 +134,16 @@ void net_handler(u_char * device , const struct pcap_pkthdr * pkhdr ,  const u_c
 
 }
 
-struct __active_inet_devices * append_inetdev(struct  __active_inet_devices  * idevs , struct __active_inet_devices *  new_inetdev) 
-{ 
-  struct __active_inet_devices * hold  = idevs ; //new_inetdev;  
-  printf("new inet device  %s \n" , new_inetdev->idev) ; 
+struct __active_inet_devices * 
+append_inetdev(struct  __active_inet_devices  * idevs , struct __active_inet_devices *  new_inetdev) 
+{
+  //! hold the previews  
+  struct __active_inet_devices * hold  = idevs ;  
+  printf("new inet device  %s \n" , new_inetdev->idev) ;
+  
+  //! permutation 
   idevs =  new_inetdev ; 
   idevs->next  = hold ; 
-  // new_inetdev->next = idevs   ; 
 
   return idevs  ;   
 }
@@ -148,4 +157,11 @@ void list_inetdevs (const struct __active_inet_devices  * inetdevs)
     printf("-> %s\n" , inetdevs_hold->idev) ; 
     inetdevs_hold =  inetdevs_hold->next ; 
   }
+}
+
+
+char * net_get_device_name(struct __active_inet_devices  *idevs)  
+{
+   
+  return   (idevs->idev && strlen(idevs->idev) > 0 )  ?  idevs->idev   : nullable  ;  
 }
